2003434e09afca20f923e7b63ae80fea24f3942e
Marcus Nyeholt <marcus@silverstripe.com.au>
2011-06-12 17:46:39 +1000

    Doco and dependency check

--- _config.php
+++ _config.php
@@ -1,2 +1,9 @@
 <?php
 
+if (($RESTRICTED_OBJECTS_DIR = basename(dirname(__FILE__))) != 'restrictedobjects') {
+	die("The restricted objects module must be installed in /restrictedobjects, not $RESTRICTED_OBJECTS_DIR");
+}
+
+if (!class_exists('MultiValueField')) {
+	die('The restricted objects module requires the multivaluefield module from http://github.com/nyeholt/silverstripe-multivaluefield');
+}
