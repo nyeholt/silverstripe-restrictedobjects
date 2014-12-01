560b39071a7e815fe3371c2471d914e287482a2e
Marcus Nyeholt <nyeholt@gmail.com>
2014-12-02 08:14:17 +1100

    FIX restricted list re-queries to full limit
    * Fixed the restricted() method on the list decorator to ensure the returned
      list containtains as many elements as needed to match the supplied limit.
    * Fixed test cases for needed items

--- code/model/RestrictedDataList.php
+++ code/model/RestrictedDataList.php
@@ -7,7 +7,7 @@
 class RestrictedDataList extends Extension {
 	
 	public function restrict($perm = 'View') {
-		return $this->owner->filterByCallback(function ($item) use ($perm) {
+		$restrictFilter = function ($item) use ($perm) {
 			if ($item->hasExtension('Restrictable') && $item->checkPerm($perm)) {
 				return true;
 			} else if ($item->hasMethod('can' . $perm)) {
@@ -16,9 +16,64 @@ class RestrictedDataList extends Extension {
 			} else {
 				return $item->canView();
 			}
-		});
+		};
+		
+		$unrestrictedCount = $this->owner->count();
+		
+		$list = $this->owner->filterByCallback($restrictFilter);
+		
+		$listCount = $list->count();
+		
+		if ($unrestrictedCount == $listCount) {
+			return $list;
+		}
+		
+		// otherwise we need to get the 
+		$limitInfo = $this->owner->dataQuery()->getFinalisedQuery()->getLimit();
+		if (isset($limitInfo['limit'])) {
+			$limit = $limitInfo['limit'];
+		} else {
+			$limit = 0;
+		}
+		
+		if ($limit && $unrestrictedCount == $limit) {
+			$targetNumber = (int) trim($limit);
+			$count = $list->count();
+			$lastCount = $count;
+			$newOffset = $limitInfo['start'];
+			while ($count < $targetNumber) {
+				$nextOffset = $newOffset = $newOffset + $targetNumber;
+				$nextList = $this->owner->limit($targetNumber, $newOffset);
+				foreach ($nextList as $item) {
+					$nextOffset++;
+					if ($item->hasExtension('Restrictable')) {
+						if ($item->checkPerm($perm)) {
+							$list->push($item);
+						}
+					} else if ($item->canView()) {
+						$list->push($item);
+					}
+
+					if ($list->count() >= $targetNumber) {
+						break;
+					}
+				}
+
+				$count = $list->count();
+				// if we haven't actually increased, we'll just bail here
+				if ($lastCount == $count) {
+					break;
+				}
+			}
+			
+			$list->QueryOffset = $nextOffset;
+		}
+		
+		
+		
+		return $list;
 	}
-	
+
 	public function restrictedByID($id, $perm = 'View') {
 		$item = $this->owner->byID($id);
 		if (!$item) {
