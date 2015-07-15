7cc002f92209d7b7be284853212312174c780321
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-07-15 11:47:56 +1000

    FIX BasicAuth permission failures
    * Added an extension for projects to use that prevents BasicAuth member
      write() permission failure errors when basic auth is triggered

---
+++ code/extensions/BasicAuthEnablerExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author marcus
+ */
+class BasicAuthEnablerExtension extends Extension {
+	static $do_reenable = false;
+	
+	public function onBeforeInit() {
+		// see if we have a requirement for basic auth. If so, and there's no current user, we should disable
+		// restrictions on member objects as it will break the login process. Can re-enable afterwards
+		if (Config::inst()->get('BasicAuth', 'entire_site_protected') && !Member::currentUserID()) {
+			if (Restrictable::get_enabled() && Member::has_extension('Restrictable')) {
+				Restrictable::set_enabled(false);
+				self::$do_reenable = true;
+			}
+			
+		}
+	}
+	
+	public function onAfterInit() {
+		if (self::$do_reenable) {
+			Restrictable::set_enabled(true);
+		}
+	}
+}
