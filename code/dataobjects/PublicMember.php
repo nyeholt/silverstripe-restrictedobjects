61f58430377a27ca96b932b7811a84e4e97f870e
jcarter <josh@symbiote.com.au>
2020-06-22 15:21:36 +1000

    add(7.4): Support for PHP 7.4

--- code/dataobjects/PublicMember.php
+++ code/dataobjects/PublicMember.php
@@ -2,31 +2,31 @@
 
 /**
  * A user that represents a public user of the system
- * 
- * NOTE: This is not actively being used anywhere just yet... an explicit 'public' 
- * flag is currently being used to indicate 'public view'. 
- * 
+ *
+ * NOTE: This is not actively being used anywhere just yet... an explicit 'public'
+ * flag is currently being used to indicate 'public view'.
+ *
  * This might need to be considered later when public users can do things
  * like creating items, but realistically that should be managed using
  * runAs blocks of code
- * 
+ *
  * @deprecated
  * @author marcus@silverstripe.com.au
  * @license BSD License http://silverstripe.org/bsd-license/
  */
 class PublicMember extends Member
 {
-    
+
     public function __construct($record = null, $isSingleton = false)
     {
         parent::__construct($record, $isSingleton);
-        
+
         // we're explicitly setting our ID to -1. This is detected by the
         // permission service management code for public permission application
         $this->ID = -1;
     }
-    
-    public function write()
+
+    public function write($showDebug = false, $forceInsert = false, $forceWrite = false, $writeComponents = false)
     {
         throw new Exception("Cannot save public member");
     }
